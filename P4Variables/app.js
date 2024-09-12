e72883460f141f307a47ac06efe11dc3872b6ee7
Nazmengstu <nansymengstu@yahoo.com>
2024-09-12 08:37:07 +0200

    p4

---
+++ P4Variables/app.js
@@ -0,0 +1,17 @@
+let numberOfBottles = 10;
+const line1 = "green bottles hanging on the wall";
+const line2 = "and if one green bottle, should accidentally fall, there will be";
+
+// Logga ut värdena i följd
+console.log(numberOfBottles);
+console.log(line1);
+console.log(numberOfBottles);
+console.log(line1);
+console.log(line2);
+
+// Minska antal flaskor med 1
+numberOfBottles -= 1;
+
+// Logga ut det nya antalet flaskor och raden igen
+console.log(numberOfBottles);
+console.log(line1);
