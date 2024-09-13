9fb5a43c6160dcb01033b159b719cc672136564a
Nazmengstu <nansymengstu@yahoo.com>
2024-09-13 12:41:23 +0200

    p5

--- P4Variables/app.js
+++ P4Variables/app.js
@@ -1,4 +1,6 @@
-let numberOfBottles=10;
+let numberOfBottles;
+numberOfBottles=10;
+
 const line1 = "green bottles hanging on the wall";
 const line2 = "and if one green bottle, should accidentally fall, there will be"
 
---
+++ untitled, P5Operators/app.js
@@ -0,0 +1,50 @@
+let x=72;
+let y=3.6
+console.log("Hastighet i M/S är: ",x/y)
+
+// nr 2
+let xx =3;
+let yy=60;
+console.log("Omvandling av minuter till sekunder:",xx*yy)
+
+// nr 3
+let nummer1=11;
+let nummer2 =4;
+console.log("division av dessa nummer är:", nummer1/nummer2)
+
+// nr 4
+let a=11;
+let b=4;
+console .log("remainder of dividing:",a%b)
+
+//nr 5
+let radie=2;
+let PI=3.14
+console.log("the area of circle:",radie*radie*PI)
+
+// nr 6
+let A=11;
+console.log(-A)
+
+// nr 7
+//BMI=KG/height*height
+let kg =70;
+let height=3.3127;
+console.log("BMI:",kg/height);
+
+// nr 8
+let z=3;
+let c=4;
+let zc=3*3+4*4;
+let svar=Math.sqrt(zc);
+console.log(zc);
+console.log(svar)
+
+// nr 9
+let s=111;
+let m=Math.floor(s/60);
+let l=s%60;
+console.log(m+"minute (s) and "+l +"second(s).")
+
+
+
