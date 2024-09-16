4bc9630bead2e7438b96ad74312a908e93fbb50a
Nazmengstu <nansymengstu@yahoo.com>
2024-09-16 21:00:51 +0200

    task

---
+++ P6 Types/app.js
@@ -0,0 +1,5 @@
+
+let numberOfbottles=10;
+console.log(`${numberOfbottles} green bottles hangning on the well, and if one green bottle should accidentlly fall, there will be ${--numberOfbottles} green bottles hanging on the wall.`);
+
+
---
+++ lektion 7/app.js
@@ -0,0 +1,5 @@
+let firstname="Ben";
+    let height=172;
+let nettWorth=-94635273637763
+let isAlive= true
+const person={firstname:"Ting",latsnime:"Ben"}
