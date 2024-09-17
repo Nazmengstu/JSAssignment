13c48857f98731cf09d0d8729e2431718a187212
Nazmengstu <nansymengstu@yahoo.com>
2024-09-17 14:58:09 +0200

    funktioner html

--- html dom/js/app.js
+++ html dom/js/app.js
@@ -1,17 +1,20 @@
-console.log("Hello World!");
+
 const textField = document.getElementById("text0");
-textField.innerHTML = "Nazret!";
+textField.innerHTML =multiplyByselfe(99)
+textField.style.fontSize = "30px"
+
 
 const headerText = document.getElementById("header5");
-headerText.innerHTML = "Min första websida";
-
-headerText.style.color= "red"
-headerText.style.fontSize="30px";
-headerText.style.textAlign="center"
-headerText.style.backgroundColor= "black"
+headerText.innerHTML = " Nazret"
 
 const buttonText = document.getElementById("button0");
-buttonText.innerHTML = "välkommen till min websida";
-buttonText.style.color= "blue"
-buttonText.style.fontSize="20px";
+buttonText.innerHTML = " välkommen "
 
+
+
+  // öva funktioner ,
+function multiplyByselfe(nummer,) {
+  return nummer*nummer;
+
+}
+console.log(multiplyByselfe(4));
