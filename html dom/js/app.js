6b8cb5508aec3b2b16d35eece144d1d114e4030b
Nazmengstu <nansymengstu@yahoo.com>
2024-09-17 14:30:11 +0200

    html doc

---
+++ html dom/js/app.js
@@ -0,0 +1,17 @@
+console.log("Hello World!");
+const textField = document.getElementById("text0");
+textField.innerHTML = "Nazret!";
+
+const headerText = document.getElementById("header5");
+headerText.innerHTML = "Min första websida";
+
+headerText.style.color= "red"
+headerText.style.fontSize="30px";
+headerText.style.textAlign="center"
+headerText.style.backgroundColor= "black"
+
+const buttonText = document.getElementById("button0");
+buttonText.innerHTML = "välkommen till min websida";
+buttonText.style.color= "blue"
+buttonText.style.fontSize="20px";
+
