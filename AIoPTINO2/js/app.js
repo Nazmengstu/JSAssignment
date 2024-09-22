884a900db3e516f0cedc11b14a903b57dba09fb5
Nazmengstu <nansymengstu@yahoo.com>
2024-09-22 21:33:28 +0200

    Ai Option2

--- AIoPTINO2/js/app.js
+++ AIoPTINO2/js/app.js
@@ -1,40 +1,59 @@
 let button0 = document.getElementById("button0");
 let button1 = document.getElementById("button1");
 let button2 = document.getElementById("button2");
+let game=document.getElementById("game");
+let AiChoice=0;
+let val="";
 
-let Choice=["Rock", "Paper", "Siccors"]
-
-
-
-
-button0.addEventListener("click", function() { HandlebuttonClick("Rock"); });
-button1.addEventListener("click", function() { HandlebuttonClick("Paper"); });
-button2.addEventListener("click", function() { HandlebuttonClick("Siccors"); });
-
-
+button0.addEventListener("click", function(){HandlebuttonClick( "Rock");getRandomNumberForAI();choiceai()})
+button1.addEventListener("click", function(){HandlebuttonClick( "Paper");getRandomNumberForAI();choiceai()})
+button2.addEventListener("click", function(){HandlebuttonClick( "Siccor");getRandomNumberForAI();choiceai()})
 
 function HandlebuttonClick(choice) {
+  val = choice;
   // Directly display the player's choice in the HTML element
   document.getElementById('playerChoiceDisplay').innerHTML = `You chose: ${choice}`;
 }
 
+
+
+
 function getRandomNumberForAI(){
-  AiChoice=Math.floor( Math.random() * 3 );
+  AiChoice=Math.floor( Math.random() * 3 )+1;
 }
 
-function getRandomNumberForPlayer(){
-  PlayerChoice=Math.floor( Math.random() * 3 );
+
+//1=Siccor 2=Rock 3=Paper
+function choiceai() {
+  if (AiChoice == 1 && val=="Rock") {
+    game.innerHTML = "You win, AI chose siccor"
+  } else if (AiChoice == 1 && val=="Paper") {
+    game.innerHTML = "You loose, AI chose siccor"
+  }
+  else if (AiChoice == 1 && val=="Siccor") {
+    game.innerHTML = "Its a draw, AI chose siccor"
+  }
+  else if (AiChoice == 2 && val=="Rock") {
+    game.innerHTML = "Its a draw, AI chose Rock"
+  } else if (AiChoice == 2 && val=="Paper") {
+    game.innerHTML = "You win, AI chose Rock"
+  }
+  else if (AiChoice == 2 && val=="Siccor") {
+    game.innerHTML = "You loose, AI chose Rock"
+  }
+
+  else if (AiChoice == 3 && val=="Rock") {
+    game.innerHTML = "You loose, AI chose Paper"
+  } else if (AiChoice == 3 && val=="Paper") {
+    game.innerHTML = "Its a draw, AI chose Paper"
+  }
+  else if (AiChoice == 3 && val=="Siccor") {
+    game.innerHTML = "You win, AI chose Paper"
+  }
+
+
+  else {
+    game.innerHTML = "Error"
+  }
 }
 
-function Play(){
-  if AiChoice==PlayerChoice
-    Resulte.innerHTML = "It's Draw"
-else if (
-  (PlayerChoice === 'rock' && AiChoice === 'scissors') ||
-  (PlayerChoice === 'paper' && AiChoice === 'rock') ||
-  (PlayerChoice === 'scissors' && AiChoice === 'paper')
-) {
-  return "You win!";
-} else {
-  return "AI wins!";
-}}
